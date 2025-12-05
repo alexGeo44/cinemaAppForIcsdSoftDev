dcdbb2e5331e21d9f2971c4539e9803ed8f03945
Alex Geo <alexmangeo44@gmail.com>
2025-12-05 13:12:59 +0200

    full

---
+++ src/main/java/application/CinemaManagerApplication.java
@@ -0,0 +1,12 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaManagerApplication.class, args);
+    }
+}
